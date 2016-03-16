d9f2d3df27ea460ce4949057ebc954a2107132ff
WesleyTsai <tsai.wesley@gmail.com>
2016-03-15 20:50:44 -0700

    finished gouraud shading

--- glsl/gouraud.vs.glsl
+++ glsl/gouraud.vs.glsl
@@ -13,18 +13,22 @@ varying vec3 color;
 vec3 getReflection() {
     // R = 2(N(NL)) - L
     vec3 N = normal;
-    vec3 L = normalize(position - lightPosition);
+    vec3 L = normalize(normalize(position) - normalize(lightPosition));
     float NL = dot(N, L);
     vec3 NNL = NL * N;
-    return (2.0 * NNL) - L;
+    return normalize((2.0 * NNL) - L);
 }
 
 void main() {
     interpolatedNormal = normal;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 
-    vec3 specularLight = kSpecular * lightColor * pow(dot(normal, getReflection()), shininess);
-    color = specularLight;
+    vec3 R = getReflection();
+    vec3 L = normalize(normalize(position) - normalize(lightPosition));
+    // Is = Ks * Il * (VR)^n
+    vec3 ambientLight = kAmbient * ambientColor;
+    vec3 specularLight = kSpecular * lightColor * pow(dot(normal, R), shininess);
+    vec3 diffuseLight = kDiffuse * lightColor * dot(normal, L);
+    color = ambientLight + specularLight + diffuseLight;
 }
 
-// Is = Ks Il (VR)^n
